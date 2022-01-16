0075163792941be5bdfaef4de68ca703aa567fdc
aerlevsedi <sobasmarta@gmail.com>
2022-01-16 16:29:43 +0100

    Adding films to DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,165 @@
-import React from 'react';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
-const App = () => {
-  return (
-    <div>
-      <h1>In progress</h1>
-    </div>
-  );
+function App() {
+	const [title, setTitle] = useState("");
+	const [director, setDirector] = useState("");
+	const [genre, setGenre] = useState("");
+	const [release_year, setReleaseYear] = useState(0);
+	const [duration, setDuration] = useState(0);
+
+	const [newDuration, setNewDuration] = useState(0);
+
+	const [filmList, setFilmList] = useState([]);
+
+	const addFilm = () => {
+		Axios.post("http://localhost:3001/createFilm", {
+			title: title,
+			director: director,
+			genre: genre,
+			release_year: release_year,
+			duration: duration,
+		}).then(() => {
+			console.log("succes");
+			setFilmList([
+				...filmList,
+				{
+					title: title,
+					director: director,
+					genre: genre,
+					release_year: release_year,
+					duration: duration,
+				},
+			]);
+		});
+	};
+
+	const getFilms = () => {
+		Axios.get("http://localhost:3001/films").then((response) => {
+			console.log(response);
+			setFilmList(response.data);
+		});
+	};
+
+	const updateFilmDuration = (id) => {
+		Axios.put("http://localhost:3001/updateFilm", {
+			duration: newDuration,
+			id: id,
+		}).then((response) => {
+			alert("update");
+			setFilmList(
+				filmList.map((val) => {
+					return val.id == id
+						? {
+								id: val.id,
+								title: val.title,
+								director: val.director,
+								genre: val.genre,
+								release_year: val.release_year,
+								duration: newDuration,
+						  }
+						: val;
+				})
+			);
+		});
+	};
+
+	const deleteFilm = (id) => {
+		Axios.delete(`http://localhost:3001/deleteFilm/${id}`).ther((response) => {
+			setFilmList(
+				filmList.filter((val) => {
+					return val.id != id;
+				})
+			);
+		});
+	};
+
+	return (
+		<div className="App">
+			<div className="information">
+				<label>Title:</label>
+				<input
+					type="text"
+					onChange={(event) => {
+						setTitle(event.target.value);
+					}}
+				/>
+				<label>Director:</label>
+				<input
+					type="text"
+					onChange={(event) => {
+						setDirector(event.target.value);
+					}}
+				/>
+				<label>Genre:</label>
+				<input
+					type="text"
+					onChange={(event) => {
+						setGenre(event.target.value);
+					}}
+				/>
+				<label>Release Year:</label>
+				<input
+					type="number"
+					onChange={(event) => {
+						setReleaseYear(event.target.value);
+					}}
+				/>
+				<label>Duration:</label>
+				<input
+					type="number"
+					onChange={(event) => {
+						setDuration(event.target.value);
+					}}
+				/>
+
+				<button onClick={addFilm}>Add Film</button>
+			</div>
+
+			<div className="employees">
+				<button onClick={getFilms}>Show Films</button>
+				{filmList.map((val, key) => {
+					console.log("want to show");
+					return (
+						<div className="employee">
+							<div>
+								<h3>Title: {val.title}</h3>
+								<h3>Director: {val.director}</h3>
+								<h3>Genre: {val.genre}</h3>
+								<h3>Release Year: {val.release_year}</h3>
+								<h3>Duration: {val.duration}</h3>
+							</div>
+							<div>
+								{" "}
+								<input
+									type="text"
+									placeholder="2000..."
+									onChange={(event) => {
+										setNewDuration(event.target.value);
+									}}
+								/>
+								<button
+									onClick={() => {
+										updateFilmDuration(val.id);
+									}}
+								>
+									Update
+								</button>
+								<button
+									onClick={() => {
+										deleteFilm(val.id);
+									}}
+								>
+									Delete
+								</button>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,31 +1,97 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mysql from 'mysql';
+import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: '',
-    database: 'cinema',
+	user: "root",
+	host: "localhost",
+	password: "",
+	database: "cinema",
 });
 
 db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('Connected');
-})
+	if (err) {
+		throw err;
+	}
+	console.log("Connected");
+});
 
-app.post('/create', (req, res) => {
-    db.query("INSERT INTO ticket (id_order, id_screening, id_seat, kind, price) values ('2', '6', '5', 'normal', '20')");
-    db.query("insert into movies (title, director, release_year, genre) values ('Film3', '1', '1995', 'Horror'),");
-})
+app.post("/createFilm", (req, res) => {
+	const title = req.body.title;
+	const director = req.body.director;
+	const genre = req.body.genre;
+	const release_year = req.body.release_year;
+	const duration = req.body.duration;
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+	db.query(
+		"INSERT INTO film (title, director, genre, release_year, duration) VALUES (?,?,?,?,?)",
+		[title, director, genre, release_year, duration],
+		(err, result) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.send("Values inserted");
+			}
+		}
+	);
+});
+
+app.get("/films", (req, res) => {
+	db.query("SELECT * FROM film", (err, result) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(result);
+		}
+	});
+});
+
+app.put("/updateFilm", (req, res) => {
+	const id = req.body.id;
+	const duration = req.body.duration;
+	db.query(
+		"UPDATE film SET duration = ? WHERE id = ?",
+		[duration, id],
+		(err, result) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.send(result);
+			}
+		}
+	);
+});
+
+app.delete("/deleteFilm/:id", (req, res) => {
+	const id = req.params.id;
+	console.log(id);
+	db.query("DELETE FROM film WHERE id = ?", id, (err, result) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(result);
+		}
+	});
+});
+
+app.post("/create", (req, res) => {
+	db.query(
+		"INSERT INTO ticket (id_order, id_screening, id_seat, kind, price) values ('2', '6', '5', 'normal', '20')"
+	);
+	db.query(
+		"insert into movies (title, director, release_year, genre) values ('Film3', '1', '1995', 'Horror'),"
+	);
+});
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.listen(3001, () => {
-    console.log("Running on port 3001");
-})
+	console.log("Running on port 3001");
+});
